709891086df8a389e9be4252e8bb90e9932be817
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-05-11 13:54:32 -0600

    Started working on the project. Added the header section, and started on the regularCalculator function.

---
+++ Calculator-Master/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="welcome">
+        <h2>Welcome to Calculator Master</h2>
+        <h3>Please select a calculator mode below</h3>
+      </div>
+      <div id="calculator"></div>
+    </>
+  )
+}
+
+export default App
---
+++ Calculator-Master/src/components/header.tsx
@@ -0,0 +1,11 @@
+import '../index.css'
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <strong>The Calculator Master</strong>
+      </div>
+    </header>
+  )
+}
